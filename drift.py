1be5c4fffd73ce6b0d00d323f850a172c29a0671
Taylor Hutchison <aibhleog@tamu.edu>
2019-09-13 16:08:58 -1000

    Updating drift.py

--- drift.py
+++ drift.py
@@ -11,7 +11,8 @@ import pandas as pd
 import shutil
 import os
 
-__version__ = '2019sep4'
+__author__ = 'Taylor Hutchison'
+__year__ = '2019'
 
 import sys
 if sys.version_info[0] < 3:
@@ -40,7 +41,6 @@ class Drift:
     col_start = 0   # the xvalue for the first column (avoid skylines please)
     col_end = 0     # the xvalue for the last column (avoid skylines please)
     
-    
     # LIST OF RAW FRAMES FOR CHOSEN MASK
     # -- full list of frames
     def mask_frames(self,home=''):
@@ -50,9 +50,13 @@ class Drift:
         '''
         path = home+'%s/'%self.date
         
+        # converts the mask name into the format for the file names
+        mfile = dt.strptime(self.date,'%Y%b%d').strftime('%y%m%d')
+        #print(mfile)
+        
         allfiles = np.asarray(os.listdir(path))
-        ending = [f[-4:] for f in allfiles] # fast way to ignore other files
-        raw_frames = allfiles[np.asarray(ending) == 'fits']
+        mfiles = [f[:7] for f in allfiles] # fast way to ignore other files
+        raw_frames = allfiles[np.asarray(mfiles) == 'm'+mfile]
         raw_frames = np.sort(raw_frames)
         
         mask_frames = []
@@ -178,87 +182,3 @@ class Drift:
         plt.show()
         plt.close('all')
     
-    
-
-# TESTING ON 2018DEC12 DATA for COSMOS
-test = Drift()
-
-test.date = '2018apr25'
-test.mask = 'EGSMask1'
-test.dither = 1.25
-test.band = 'J'
-
-test.row_start = 1729
-test.row_end = 1766
-test.col_start = 972
-test.col_end = 982
-
-
-# -- getting list of files for both dithers
-#home = '/home/aibhleog/Desktop/observing/'
-home = '/run/media/aibhleog/eve_lily/observing-keck2018/'
-all_frames = test.mask_frames(home=home)
-nod_A, nod_B = test.split_dither(all_frames,home=home)
-
-
-# ------------- measuring the fits ------------- #
-# ---------------------------------------------- #
-mean, A, sig, num = test.fit_all(nod_A,home=home)   
-df_A = pd.DataFrame({'frame':num,'cen':mean,'A':A,'sig':sig})
-
-mean, A, sig, num = test.fit_all(nod_B,home=home)   
-df_B = pd.DataFrame({'frame':num,'cen':mean,'A':A,'sig':sig})
-# ---------------------------------------------- #
-
-df_A['offset'] = (df_A.loc[0,'cen']-df_A.cen.values) * 0.18 # "/pixel
-df_B['offset'] = (df_B.loc[0,'cen']-df_B.cen.values) * 0.18 # "/pixel
-df_A['seeing'] = df_A.sig.values * 2.35 * 0.18 # "/pixel
-df_B['seeing'] = df_B.sig.values * 2.35 * 0.18 # "/pixel
-
-
-# LOOKING AT THE DATA
-
-# -- OFFSET AS FUNCTION OF TIME (i.e., FRAME) -- #
-# ---------------------------------------------- #
-plt.figure(figsize=(9,6))
-plt.scatter(df_A.frame,df_A.offset,edgecolor='k',s=60,label='Nod A')
-plt.scatter(df_B.frame,df_B.offset,edgecolor='k',marker='^',s=60,label='Nod B')
-
-plt.text(0.975,0.05,'Mask: %s'%(test.mask),ha='right',\
-         transform=plt.gca().transAxes,fontsize=15)
-plt.text(0.975,0.1,'Date: %s'%(test.date),ha='right',\
-         transform=plt.gca().transAxes,fontsize=16)
-
-plt.legend(loc=2)
-plt.xlabel('frame number')
-plt.ylabel('$y_0 - y$ ["]')
-plt.ylim(-0.2,0.4)
-
-plt.tight_layout()
-plt.show()
-plt.close()
-# ============================================== #
-
-
-# -- SEEING MAP AS FUNCTION OF TIME (i.e., FRAME) -- #
-# -------------------------------------------------- #
-plt.figure(figsize=(9,6))
-plt.scatter(df_A.frame,df_A.seeing,edgecolor='k',s=60,label='Nod A')
-plt.scatter(df_B.frame,df_B.seeing,edgecolor='k',marker='^',s=60,label='Nod B')
-
-plt.text(0.975,0.92,'Mask: %s'%(test.mask),ha='right',\
-         transform=plt.gca().transAxes,fontsize=15)
-plt.text(0.975,0.88,'Date: %s'%(test.date),ha='right',\
-         transform=plt.gca().transAxes,fontsize=16)
-
-plt.legend(loc=2)
-plt.xlabel('frame number')
-plt.ylabel('seeing ["]')
-plt.ylim(0.4,1.6)
-
-plt.tight_layout()
-plt.show()
-plt.close()
-# ================================================== #
-
-test.show_me_all_profiles(np.concatenate((nod_A,nod_B)),home=home)
